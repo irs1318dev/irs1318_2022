00c1a623e23de7c26af713a6c4cfc5c29fddb94f
William Rall <William Rall>
2022-03-04 19:29:08 -0800

    Remove some vision constants that are no longer relevant

--- core_robot/src/main/java/frc/robot/HardwareConstants.java
+++ core_robot/src/main/java/frc/robot/HardwareConstants.java
@@ -10,16 +10,6 @@ import frc.robot.common.robotprovider.TalonFXInvertType;
  */
 public class HardwareConstants
 {
-    //================================================= Vision ======================================================
-
-    // Vision Alignment 
-    public static final double CAMERA_PITCH = 22.5; // in degrees
-    public static final double CAMERA_X_OFFSET = 0.0; // in inches
-    public static final double CAMERA_Z_OFFSET = 18.0; // in inches
-    public static final double VISIONTARGET_Z_OFFSET = 90.25; // in inches
-    public static final double CAMERA_TO_TARGET_Z_OFFSET = HardwareConstants.VISIONTARGET_Z_OFFSET - HardwareConstants.CAMERA_Z_OFFSET;
-    public static final double CAMERA_YAW = 0.0; // in degrees
-
     //================================================== DriveTrain ==============================================================
 
     public static final TalonFXInvertType DRIVETRAIN_STEER_MOTOR1_INVERT = TalonFXInvertType.Clockwise;
